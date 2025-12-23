bd017be6c34430f00c48e8895a55c7ccf1ad8dff
Bayu Sukma <renmizuki38@gmail.com>
2025-12-23 20:48:56 +0700

    Update UserSeeder.php

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -1,1 +1,40 @@
 
+<?php
+   
+   namespace App\Database\Seeds;
+   
+   use CodeIgniter\Database\Seeder;
+   
+   class UserSeeder extends Seeder
+   {
+       public function run()
+       {
+           // Data untuk Admin
+           $data = [
+               [
+                   'username' => 'admin',
+                   'password' => password_hash('admin123', PASSWORD_BCRYPT),
+                   'role'     => 'admin',
+                   'name'     => 'Administrator',
+                   'email'    => 'admin@sonataviolin.com'
+               ],
+               [
+                   'username' => 'operator',
+                   'password' => password_hash('operator123', PASSWORD_BCRYPT),
+                   'role'     => 'operator',
+                   'name'     => 'Operator',
+                   'email'    => 'operator@sonataviolin.com'
+               ],
+               [
+                   'username' => 'instruktur',
+                   'password' => password_hash('instruktur123', PASSWORD_BCRYPT),
+                   'role'     => 'instruktur',
+                   'name'     => 'Instruktur',
+                   'email'    => 'instruktur@sonataviolin.com'
+               ]
+           ];
+   
+           // Insert data
+           $this->db->table('users')->insertBatch($data);
+       }
+   }
